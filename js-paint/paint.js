c0ce3edff5d6eeba4693360b258713b0f4b9c256
plsr <christianpoplawski@gmail.com>
2013-10-27 00:18:21 +0200

    User can now choose between two drawing tools
    Still, none of the code is mine

--- js-paint/paint.js
+++ js-paint/paint.js
@@ -1,25 +1,24 @@
-/* © 2009 ROBO Design
+/* Â© 2009 ROBO Design
  * http://www.robodesign.ro
  */
 
 // Keep everything in anonymous function, called on window load.
 if(window.addEventListener) {
-  //on load call function()
 window.addEventListener('load', function () {
   var canvas, context;
 
-  // Initialization sequence.
+  // The active tool instance.
+  var tool;
+  var tool_default = 'rect';
+
   function init () {
     // Find the canvas element.
     canvas = document.getElementById('imageView');
-
-    //is a canvas?
     if (!canvas) {
       alert('Error: I cannot find the canvas element!');
       return;
     }
 
-    //can get context?
     if (!canvas.getContext) {
       alert('Error: no canvas.getContext!');
       return;
@@ -32,52 +31,29 @@ window.addEventListener('load', function () {
       return;
     }
 
-    //Pencil tool instance
-    tool = new tool_pencil();
+    // Get the tool select input.
+    var tool_select = document.getElementById('dtool');
+    if (!tool_select) {
+      alert('Error: failed to get the dtool element!');
+      return;
+    }
+    tool_select.addEventListener('change', ev_tool_change, false);
 
-    // Attach the mousemove, mousedown and mouseup event handler.
-    canvas.addEventListener('mousemove', ev_canvas, false);
+    // Activate the default tool.
+    if (tools[tool_default]) {
+      tool = new tools[tool_default]();
+      tool_select.value = tool_default;
+    }
+
+    // Attach the mousedown, mousemove and mouseup event listeners.
     canvas.addEventListener('mousedown', ev_canvas, false);
-    canvas.addEventListener('mouseup', ev_canvas, false);
+    canvas.addEventListener('mousemove', ev_canvas, false);
+    canvas.addEventListener('mouseup',   ev_canvas, false);
   }
 
-  //This painting tool works like a drawing pancil which tracks the 
-  //mouse movements
-  function tool_pencil () {
-    var tool = this;
-    this.started = false;
-
-      //This is called when you start holding down the mouse button.
-      //This starts the pencil drawing.
-      this.mousedown = function (ev) {
-        context.beginPath();
-        context.moveTo(ev._x, ev._y);
-        tool.started = true;
-      };
-
-      //This function is called every time you move the mouse. Only draws 
-      //if tool.started is true, means when mouse button is held down
-      this.mousemove = function (ev) {
-        if (tool.started) {
-          context.lineTo(ev._x, ev._y);
-          context.stroke();
-        }
-      };
-
-      //This is called on button release.
-      this.mouseup = function (ev) {
-        if(tool.started){
-          tool.mousemove(ev);
-          tool.started = false;
-        }
-      };
-  }
-
-  // The mousemove event handler.
+  // The general-purpose event handler. This function just determines the mouse 
+  // position relative to the canvas element.
   function ev_canvas (ev) {
-
-    // The general purpose event handler.
-    //Gets the mouse position relative to the canvas element.
     if (ev.layerX || ev.layerX == 0) { // Firefox
       ev._x = ev.layerX;
       ev._y = ev.layerY;
@@ -86,14 +62,96 @@ window.addEventListener('load', function () {
       ev._y = ev.offsetY;
     }
 
-    //Call the event handler of the function
+    // Call the event handler of the tool.
     var func = tool[ev.type];
-    if(func) {
+    if (func) {
       func(ev);
     }
   }
 
+  // The event handler for any changes made to the tool selector.
+  function ev_tool_change (ev) {
+    if (tools[this.value]) {
+      tool = new tools[this.value]();
+    }
+  }
+
+
+  // This object holds the implementation of each drawing tool.
+  var tools = {};
+
+  // The drawing pencil.
+  tools.pencil = function () {
+    var tool = this;
+    this.started = false;
+
+    // This is called when you start holding down the mouse button.
+    // This starts the pencil drawing.
+    this.mousedown = function (ev) {
+        context.beginPath();
+        context.moveTo(ev._x, ev._y);
+        tool.started = true;
+    };
+
+    // This function is called every time you move the mouse. Obviously, it only 
+    // draws if the tool.started state is set to true (when you are holding down 
+    // the mouse button).
+    this.mousemove = function (ev) {
+      if (tool.started) {
+        context.lineTo(ev._x, ev._y);
+        context.stroke();
+      }
+    };
+
+    // This is called when you release the mouse button.
+    this.mouseup = function (ev) {
+      if (tool.started) {
+        tool.mousemove(ev);
+        tool.started = false;
+      }
+    };
+  };
+
+  // The rectangle tool.
+  tools.rect = function () {
+    var tool = this;
+    this.started = false;
+
+    this.mousedown = function (ev) {
+      tool.started = true;
+      tool.x0 = ev._x;
+      tool.y0 = ev._y;
+    };
+
+    this.mousemove = function (ev) {
+      if (!tool.started) {
+        return;
+      }
+
+      var x = Math.min(ev._x,  tool.x0),
+          y = Math.min(ev._y,  tool.y0),
+          w = Math.abs(ev._x - tool.x0),
+          h = Math.abs(ev._y - tool.y0);
+
+      context.clearRect(0, 0, canvas.width, canvas.height);
+
+      if (!w || !h) {
+        return;
+      }
+
+      context.strokeRect(x, y, w, h);
+    };
+
+    this.mouseup = function (ev) {
+      if (tool.started) {
+        tool.mousemove(ev);
+        tool.started = false;
+      }
+    };
+  };
+
   init();
+
 }, false); }
 
-// vim:set spell spl=en fo=wan1croql tw=80 ts=2 sw=2 sts=2 sta et ai cin fenc=utf-8 ff=unix:
+
