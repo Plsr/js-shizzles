8f0bd512e339a7952214080e5a4253c875713fb4
plsr <christianpoplawski@gmail.com>
2013-11-05 14:27:36 +0100

    Fixed the borders, impelemted "Quest"
    Some other stuf tested with canvas width

--- goblin-catcher/game.js
+++ goblin-catcher/game.js
@@ -1,6 +1,9 @@
 // Create the canvas
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
+console.log("Width: " + windowWidth + " | " + "Height: " + windowHeight);
 canvas.width = 512;
 canvas.height = 480;
 document.body.appendChild(canvas);
@@ -22,6 +25,14 @@ heroImage.onload = function () {
 };
 heroImage.src = "hero.png";
 
+// Quest image
+var questReady = false;
+var questImage = new Image();
+questImage.onload = function () {
+	questReady = true;
+};
+questImage.src = "hero.png";
+
 // Monster image
 var monsterReady = false;
 var monsterImage = new Image();
@@ -36,6 +47,10 @@ var hero = {
 };
 var monster = {
 	speed: 128
+};
+
+var quest = {
+
 };
 var monstersCaught = 0;
 
@@ -54,6 +69,11 @@ addEventListener("keyup", function (e) {
 var setHero = function () {
 	hero.x = canvas.width / 2;
 	hero.y = canvas.height / 2;
+};
+
+var setQuest = function () {
+	quest.x = 100;
+	quest.y = 100;
 }
 
 // Randomly place a monster
@@ -66,16 +86,16 @@ var setMonster = function () {
 
 // Update game objects
 var update = function (modifier) {
-	if (38 in keysDown && hero.y > 0) { // Player holding up
+	if (38 in keysDown && hero.y > 32) { // Player holding up
 		hero.y -= hero.speed * modifier;
 	}
-	if (40 in keysDown && hero.y < canvas.height - 32) { // Player holding down
+	if (40 in keysDown && hero.y < canvas.height - 64) { // Player holding down
 		hero.y += hero.speed * modifier;
 	}
-	if (37 in keysDown && hero.x > 0) { // Player holding left
+	if (37 in keysDown && hero.x > 32) { // Player holding left
 		hero.x -= hero.speed * modifier;
 	}
-	if (39 in keysDown && hero.x < canvas.width -32) { // Player holding right
+	if (39 in keysDown && hero.x < canvas.width - 64) { // Player holding right
 		hero.x += hero.speed * modifier;
 	}
 
@@ -103,30 +123,27 @@ var monsterMove = function(modifier) {
 	if(newPosition) {
 		newPosition = false;
 
-		
-
 		randomX = parseInt(Math.floor(Math.random() * (canvas.width - 32)));
-		randomY = parseInt(Math.floor(Math.random() * (canvas.height - 32)));
-		
+		randomY = parseInt(Math.floor(Math.random() * (canvas.height - 32)));	
 	}
 	
 
-		if(monster.x < randomX && monster.x > 0) {
+		if(monster.x < randomX && monster.x > 32) {
 			monster.x += monster.speed * modifier;
 		}
 		if(monster.x > randomX && monster.x < canvas.width - 32) {
 			monster.x -= monster.speed * modifier;
 		}
-		if(monster.y < randomY && monster.y > 0) {
+		if(monster.y < randomY && monster.y > 32) {
 			monster.y += monster.speed * modifier;
 		}
 		if(monster.y > randomY && monster.y < canvas.height -32) {
 			monster.y -= monster.speed * modifier;
 		}
-		console.log("x: " + parseInt(monster.x) + " | " + "y: " + parseInt(monster.y));
+		//console.log("x: " + parseInt(monster.x) + " | " + "y: " + parseInt(monster.y));
 		if(parseInt(monster.y) == (randomY - 1)  && parseInt(monster.x) == (randomX - 1)){
 			newPosition = true;
-			console.log("x: " + parseInt(monster.x) + " | " + "y: " + parseInt(monster.y));
+			//console.log("x: " + parseInt(monster.x) + " | " + "y: " + parseInt(monster.y));
 		}
 
 		
@@ -152,6 +169,10 @@ var render = function () {
 		ctx.drawImage(monsterImage, monster.x, monster.y);
 	}
 
+	if(questReady) {
+		ctx.drawImage(questImage, quest.x, quest.y);
+	}
+
 	// Score
 	ctx.fillStyle = "rgb(250, 250, 250)";
 	ctx.font = "24px Helvetica";
@@ -175,6 +196,7 @@ var main = function () {
 
 // Let's play this game!
 setHero();
+setQuest();
 setMonster();
 var then = Date.now();
 setInterval(main, 1); // Execute as fast as possible
